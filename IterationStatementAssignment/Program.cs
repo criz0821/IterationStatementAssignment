977fde09dd8262688e15110192204a33057ff285
Chris Mejia <cbmejia@usf.edu>
2019-01-24 17:38:51 -0500

    version 2 (adding comments)

--- IterationStatementAssignment/Program.cs
+++ IterationStatementAssignment/Program.cs
@@ -11,17 +11,22 @@ namespace IterationStatementAssignment
     {
         static void Main(string[] args)
         {
+            // prompts user to enter an integer
             Console.WriteLine("Please enter a numeric value between 1 and 100.");
             try
             {
+                // reading input from user
                 string input = Console.ReadLine();
                 
+                // creating while loop to enbale consecutive inputs
                 while (input != "EXIT")
                 {
+                    // establishing variables for user input and count
                     int numInput = int.Parse(input);
                     int num = numInput;
                     int count = 1;
 
+                    // validating user input (between 1 and 100)
                     if ((num >=1) && (num <= 100))
                     {
                         do
@@ -29,14 +34,15 @@ namespace IterationStatementAssignment
                             Console.WriteLine("You have entered " + num + ". This is the current integer value in the loop: " + count);
                             count++;
                             numInput--;
-                        }
+                        } // end of do
 
                         while (numInput > 0);
                         Console.WriteLine("");
                         Console.WriteLine("Enter another numerical value if you wish. If not then type 'EXIT'.");
                         input = Console.ReadLine();
-                    }
+                    } // end of if 
 
+                    // if user didn't enter a vlue between 1 and 100, the else statement runs
                     else
                     {
                         Console.WriteLine("");
@@ -44,20 +50,23 @@ namespace IterationStatementAssignment
                         Console.WriteLine("Try Again!");
                         input = Console.ReadLine();
                         
-                    }
-                }
+                    } // end of else
 
+                } // end of while
+
+                // when the user types 'EXIT' instead of Console.Readkey(true), this will close application instantly
                 Environment.Exit(0);
                 
-            }
+            } // end of try
 
+            // if there was an error in the try block this code will run
             catch
             {
                 Console.WriteLine("");
                 Console.WriteLine("A numeric value beween 1 and 100 was not given!");
                 Console.WriteLine("Press any key to exit the program.");
                 Console.ReadKey(true);
-            }
-        }
-    }
-}
+            } // end of catch
+        } // end of main
+    } // end of class
+} // end of namespace
